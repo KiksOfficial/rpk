613a06d315c83a53c4f51e2cb16bd258ca19e8a2
Kiks <151571662+KiksOfficial@users.noreply.github.com>
2026-06-29 15:54:44 +0300

    old code deleted

--- src/main.rs
+++ src/main.rs
@@ -28,39 +28,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let _ = update_mirrors();
     } else if operation == "-S" {
         run_install(argumendid);
-        /*let mut link_found = None;
-        let package_name = &argumendid[2];
-        let repos = ["core", "extra"];
-        for repo in repos {
-            if let Ok(link) = get_link(package_name, repo) {
-                link_found = Some(link);
-                break;
-            }
-        }
-        match link_found {
-            Some(pkg_link) => {
-                let file_name = format!("{}.tar.zst", package_name);
-                let output_path = Path::new("/tmp").join(&file_name);
-
-                println!("Downloading {}...", package_name);
-                download_file(&pkg_link, &output_path)?;
-
-                let fake_root = Path::new("/home/kiks/Proge/fake-root");
-                println!("Unpacking to fake-root...");
-                unpack_package(&output_path, fake_root)?;
-
-                let metadata_path = fake_root.join(".PKGINFO");
-                if metadata_path.exists() {
-                    let package1 = pkg_info(&metadata_path)?;
-                    println!("{:?}", &package1);
-                } else {
-                    println!("Package installed successfully (no metadata.pkg found).");
-                }
-            }
-            None => {
-                println!("Pkg '{}' not found in any repo.", package_name);
-            }
-        }*/
     }
 
     Ok(())
